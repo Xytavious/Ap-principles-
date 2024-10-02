0425794de020271dea51979c45781d8f81e83b7f
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-10-02 15:29:19 +0000

    10/2/2024

--- Q1/lp4-3.cpp
+++ Q1/lp4-3.cpp
@@ -9,7 +9,32 @@ using namespace std;
 int main()
 {
     int eggs;
+    double per;
     int n2;
 
-    std::cout<<"enter number of eggsa: "
+    std::cout<<"enter number of eggsa: ";
+    std::cin >> eggs;
+
+    if (eggs > 0 && eggs <4)
+    {
+        per = eggs * 0.50;
+        std::cout << "number of eggs " << eggs <<endl <<"the bill is " << per;
+
+    }
+    else if (eggs >= 4 && eggs < 6)
+    {
+        per = eggs * 0.45;
+        std::cout << "number of eggs " << eggs <<endl <<"the bill " << per;
+    }
+    else if (eggs >= 6 && eggs <  11)
+    {
+        per = eggs * 0.40;
+        std::cout << "number of eggs " << eggs <<endl <<"the bill " << per;
+    }
+    else if (eggs > 11)
+    {
+        per = eggs * 0.35;
+        std::cout << "number of eggs " << eggs <<endl <<"the bill " << per;
+
+    }
 }
