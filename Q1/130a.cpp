f7fcdb648dd4e0ee4786399f152721562b90f1a2
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-10-04 15:14:39 +0000

    10/4/2024

--- Q1/130a.cpp
+++ Q1/130a.cpp
@@ -6,7 +6,30 @@ using namespace std;
 // << endl: means the same as ln. (Enter)
 // std::cout << "Enter num1 "; (same as prompt)
 // std::cin >> num1; means read the previous prompt 
+
+// work note if you mod it by 2 and it reterns 0 its even and if it does not its odd 
 int main()
 {
+
+    int num1; 
+    std::cout << "Enter Number ";
+    std::cin >> num1;
+    num1 = num1 % 2;
+    int even;
+    int odd;
+    if (num1 = 0 )
+    {
+        even = num1;
+        even = even *2;
+
+    }
+    else if (num1 != 0 )
+    {
+        odd = num1;
+        odd = odd * 3 +1 ;
+        while()
+
+    }
+
 
 }
