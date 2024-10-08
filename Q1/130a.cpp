e9fe155c33528c0bfea47c4db5aa2441621c7829
Xytavious <143834355+Xytavious@users.noreply.github.com>
2024-10-08 17:00:37 +0000

    ytfdkydkjfdkyd

--- Q1/130a.cpp
+++ Q1/130a.cpp
@@ -10,46 +10,46 @@ using namespace std;
 // work note if you mod it by 2 and it reterns 0 its even and if it does not its odd
 int eve(int x)
 {
-   return x * 2;
+   return (x / 2);
 } 
 
 double od(int c)
 {
-   return (c *3) +1;
+   return (c * 3) + 1;
 }
-void loop()
+int st()
 {
-   
+    
 }
 int main()
 {
-
-    
+   
+    while (true && ){
 
     int num1; 
     std::cout << "Enter Number ";
     std::cin >> num1;
     int jimmy = num1 % 2;
-    int even;
-    int odd;
-    int res;
+    int even = 0;
+    int odd = 0;
+    int res =0;
+    
 
-
-
-
-    if (jimmy = 0 )
+    if (jimmy == 0 )
     {
-        // even = num1;
-         res = eve(num1);
+        even = num1;
+        res = eve(num1);
 
     }
     else if (jimmy != 0 )
     {
-        // odd = num1;
-         res = od(num1);
+        odd = num1;
+        res = od(num1);
+        
 
     }
     std::cout<<"Somthing "<< res;
+    }
 
 
 }
