c75e4e889c22290a706372250fe4af750abac8f1
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-01-27 16:25:17 +0000

    1/27/2025

---
+++  prog985a.py
@@ -0,0 +1,21 @@
+class Calc:
+    @staticmethod
+    def add(a, b):
+        return a+b
+    
+    @staticmethod
+    def sub(a, b):
+        return a+b
+    
+    @staticmethod
+    def mul(a, b):
+        return a+b
+    
+    @staticmethod
+    def div(a, b):
+        return a+b
+    
+    @staticmethod
+    def mod(a, b):
+        return a+b
+    
