5e222a5ef200d758433af90124d921edb4d612ca
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-02-14 18:01:46 +0000

    1qwertyuiujhgvt

--- prog985t.py
+++ prog985t.py
@@ -3,16 +3,20 @@ class mergesort:
     def sort(a):
         if len(a) > 1:
             mid = len(a)/2
-            l =a
-            r=a
+            l = a
+            r = a
             mergesort(l)
             mergesort(r)
             mergesort(a,l,r)
-
+class Merge:
     i = 0
     j =0
     k=0
-    while i < len(l) and j < len(r)
-        if l[i]< r[j]
-        a[k] = l[i]
-        i =
+    l =0
+    r=0
+    a=0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            a[k] = l[i]
+            i += 1
+        else a[k] = r[j]
