31aa5b034c0e110e0e10c44281cd3994b8a649f7
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-03-21 17:03:32 +0000

    vnm,./

---
+++ Create/some.py
@@ -0,0 +1,16 @@
+# Rental Property Manager or Inventory Manager
+# rental properties, tenants, and rent due
+# stock levels, sales, and reorders
+# need 2 functions 
+def main():
+    jhtf = 2
+
+
+
+
+
+
+
+
+def h():
+    j[6][6] = {213456},{465879}
