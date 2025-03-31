b9c8f0f0d9a53c7a481508189aec132aa378c273
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-03-31 16:31:08 +0000

    Donkey

--- Create/some.py
+++ Create/some.py
@@ -2,8 +2,20 @@
 # rental properties, tenants, and rent due
 # stock levels, sales, and reorders
 # need 2 functions 
+
+#make table with surch alg to pull data 
+# make options to add data /
 def main():
-    jhtf = 2
+    with open("dat.dat", 'r') as f: 
+        lines =f.readlines()
+
+    for i in range(0, len(lines), 3):
+      pop=0
+        
+    properties = 0
+    tenants = 0
+    rent_due =0
+# make algorithm to sort and catogrize 
 
 
 
