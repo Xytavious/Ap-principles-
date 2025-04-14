10dabf10ffb9dc07971aaaf373db920a3622307b
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-04-14 15:15:08 +0000

    commit ment issues

--- Create/some.py
+++ Create/some.py
@@ -13,29 +13,29 @@ def main():
         lines =f.readlines()
     li =[]
     for i in range(0, len(lines), 7):  #links house information all together 
-      if i + 6 < len(lines):  #fixes out of bounds
+      if i + 7 < len(lines):  #fixes out of bounds
         Adress = lines[i].strip()
         Street = lines[i+1].strip()
-        PB = lines[i+2].strip()
-        Bath = lines[i+3].strip()
-        sqr = lines[i+4].strip()
-        lot = lines[i+5].strip()
-        li.append((Adress,Street,PB,Bath,sqr,lot)) 
+        P = lines[i+2].strip()
+        B = lines[i+3].strip()
+        Bath = lines[i+4].strip()
+        sqr = lines[i+5].strip()
+        lot = lines[i+6].strip()
+        li.append((Adress,Street,P,B,Bath,sqr,lot)) 
         
     x = input("1.) Veiw all Properties  2.) Search based on street 3.) Manage properties ")
-    if x ==1:
-        df = pd.DataFrame()
-        df['Adress'] = li[0]
-        df['Street'] = li[1]
-        df['Price'] = li[2]
-        df['Bedrooms']= li[3]
-        df['Baths'] = li[4]
-        df['Street'] = li[5]
-        df['Street'] = li[6]
-        print(df)
-    elif x ==2:
-        c= mergesort
+    if x == "1":
         
+        print("hi")
+        df = pd.DataFrame(li, columns=["Address", "Street", "Price", "Bedrooms", "Bathrooms", "Square Feet", "Lot Size"])
+
+        
+        print(df)
+        
+    elif x =="2":
+        c= mergesort
+        c(li)
+        print(li)
 
         
     #properties = 0
